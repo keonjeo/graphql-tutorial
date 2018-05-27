25b1cb165e2b4d3e9d333053884276920d18adb4
Keon Ye <jnuyeyizhou@gmail.com>
2018-05-27 14:25:07 +0800

    add the create_link mutation into the mutation_type.rb file

---
+++ app/graphql/mutations/create_link.rb
@@ -0,0 +1,49 @@
+Mutations::CreateLink = GraphQL::Relay::Mutation.define do
+  name 'CreateLink'
+  description 'create a link into database'
+
+  input_field :url do
+    type !types.String
+    description 'the url for link'
+  end
+
+  input_field :description do
+    type !types.String
+    description 'the description for link'
+  end
+ 
+  return_field :link do
+    type !Types::Link
+    description 'Link that was successfully added to the database'
+  end
+ 
+  resolve -> (_, inputs, _) {
+    link = Link.create!(url: inputs[:url], description: inputs[:description])
+    {
+        link: link
+    }
+  }
+end
+
+###################   How to query this mutaion GQL    ####################
+
+# mutation createNewLink($input: CreateLinkInput!) {
+#   CreateLink(input: $input) {
+#     link {
+#       id
+#       url
+#       description
+#     }
+#   }
+# }
+
+#######################
+##  query variables  ##
+#######################
+
+# {
+#   "input": {
+#     "url": "https://www.baidu.com",
+#     "description": "Hello world"
+#   }
+# }
