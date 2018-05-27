694a3dcf8a9752f1168d8da97311dab6dd59253a
Keon Ye <jnuyeyizhou@gmail.com>
2018-05-27 12:10:09 +0800

    add link_type into the GQL types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,4 +10,11 @@ Types::QueryType = GraphQL::ObjectType.define do
       "Hello World!"
     }
   end
+
+  # queries are just represented as fields
+  field :all_links, !types[Types::LinkType] do
+    description "returns all the links in the system"
+    # resolve would be called in order to fetch data for that field
+    resolve -> (obj, args, ctx) { Link.all }
+  end
 end
